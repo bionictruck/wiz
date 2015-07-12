79ba15d8a26a6f8e23f9fc84f98d2996bc65d133
bionictruck <foofyjay@gmail.com>
2015-07-12 11:10:03 -0500

    Initial add

---
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	// Declare variables. Type Name	
+	int max;
+	int min;
+	int guess;
+	
+	void Start () {
+		StartGame ();
+		
+	}
+	
+	// Create a function - void Name () {}
+	void StartGame () {
+	
+		// Set values of the declared variables	
+		max = 1000;
+		min = 1;
+		guess = 500;
+	
+		// Simple print statements
+		print ("=============================");
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head, but don't tell me");
+		
+		print ("The highest number you can pick is " + max);
+		print ("The lowest number you can pick is " + min);
+		
+		print ("Is the number higher or lower than " + guess);
+		print ("Up arrow for higher, down arrow for lower, return if equal");
+		
+		// Adds 1 to the max so 1000 can be achieved
+		max = max + 1;
+	
+	}
+	
+	void NextGuess () {
+		guess = ( max + min) / 2;
+		print ("Higher or lower than " + guess);
+		print ("Up arrow for higher, down arrow for lower, return if equal");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	// Else If statements Receives input from a keypress and prints a result
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			min = guess;
+			NextGuess ();
+			
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			max = guess;
+			NextGuess ();
+			
+		} else if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("I won!");
+			StartGame ();
+		}
+	}
+}
